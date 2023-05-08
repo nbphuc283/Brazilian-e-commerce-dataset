a6d20a0f6bfa21dc2f06782144f2ee5ee913d951
nbphuc283 <nbphuc283@gmail.com>
2023-05-08 15:51:08 +0700

    Update brazil e-com.sql

--- brazil e-com.sql
+++ brazil e-com.sql
@@ -1,49 +1,3 @@
-/* Sellers market insights */
-SELECT osd.seller_id, 
-	osd.seller_city, 
-	COUNT(DISTINCT ood.order_id) AS total_order, 
-	COUNT(DISTINCT ood.customer_id) AS total_customer,
-	SUM(oopd.payment_value) AS total_rev,
-	ROUND(SUM(oopd.payment_value)::NUMERIC/COUNT(DISTINCT ooid.order_id), 2) AS avg_order_value,
-	ROUND(SUM(oopd.payment_value)::NUMERIC/COUNT(DISTINCT ood.customer_id), 2) AS avg_customer_value,
-	ROUND(SUM(oord.review_score)::NUMERIC/COUNT(ood.order_id), 2) AS avg_ratings
-FROM olist_sellers_dataset osd
-LEFT JOIN olist_order_items_dataset ooid
-ON osd.seller_id = ooid.seller_id
-LEFT JOIN olist_orders_dataset ood 
-ON ooid.order_id = ood.order_id
-LEFT JOIN olist_order_payments_dataset oopd
-ON ood.order_id = oopd.order_id
-LEFT JOIN olist_order_reviews_dataset oord 
-ON ood.order_id = oord.order_id 
-GROUP BY 1, 2
-ORDER BY 5 DESC
-LIMIT 20;
-
-/* Product categories market insights */
-SELECT opd.product_category_name, 
-	pcnt.product_category_name_english,
-	COUNT(DISTINCT ooid.order_id) AS total_order, 
-	COUNT(DISTINCT ood.customer_id) AS total_customer,
-	SUM(oopd.payment_value) AS total_rev,
-	ROUND(SUM(oopd.payment_value)::NUMERIC/COUNT(DISTINCT ooid.order_id), 2) AS avg_order_value,
-	ROUND(SUM(oopd.payment_value)::NUMERIC/COUNT(DISTINCT ood.customer_id), 2) AS avg_customer_value,
-	ROUND((SUM(oord.review_score)::NUMERIC/COUNT(ood.order_id)), 2) AS avg_ratings
-FROM olist_products_dataset opd 
-LEFT JOIN product_category_name_translation pcnt 
-ON opd.product_category_name = pcnt.product_category_name 
-LEFT JOIN olist_order_items_dataset ooid
-ON opd.product_id = ooid.product_id 
-LEFT JOIN olist_orders_dataset ood 
-ON ooid.order_id = ood.order_id
-LEFT JOIN olist_order_payments_dataset oopd
-ON ood.order_id = oopd.order_id
-LEFT JOIN olist_order_reviews_dataset oord 
-ON ood.order_id = oord.order_id 
-GROUP BY 1, 2
-ORDER BY 5 DESC
-LIMIT 20;
-
 /* Olist yearly business insights */
 SELECT DATE_PART('year', ood.order_purchase_timestamp) AS year,
 	DATE_PART('month', ood.order_purchase_timestamp) AS month,
@@ -52,8 +6,8 @@ SELECT DATE_PART('year', ood.order_purchase_timestamp) AS year,
 	SUM(oopd.payment_value) AS total_rev,
 	ROUND(SUM(oopd.payment_value)::NUMERIC / COUNT(DISTINCT ood.order_id), 2) AS avg_value_per_order,
 	ROUND(SUM(oopd.payment_value)::NUMERIC / COUNT(DISTINCT ood.customer_id), 2) AS avg_value_per_customer,
-	AVG(order_delivered_carrier_date - order_purchase_timestamp) AS avg_purchase_to_carrier,
-	AVG(order_delivered_customer_date - order_delivered_carrier_date) AS avg_carrier_to_customer,
+	JUSTIFY_INTERVAL(SUM(ood.order_delivered_carrier_date - ood.order_purchase_timestamp) / COUNT(DISTINCT ood.order_id)) AS avg_purchase_to_carrier,
+	JUSTIFY_INTERVAL(SUM(ood.order_delivered_customer_date - ood.order_delivered_carrier_date) / COUNT(DISTINCT ood.order_id)) AS avg_carrier_to_customer,
 	ROUND(SUM(oord.review_score)::NUMERIC / COUNT(ood.order_id), 2) AS avg_ratings
 FROM olist_orders_dataset ood
 LEFT JOIN olist_order_payments_dataset oopd 
@@ -72,8 +26,6 @@ SELECT DATE_PART('year', ood.order_purchase_timestamp) AS year,
 	SUM(oopd.payment_value) AS total_rev,
 	ROUND(SUM(oopd.payment_value)::NUMERIC / COUNT(DISTINCT ood.order_id), 2) AS avg_value_per_order,
 	ROUND(SUM(oopd.payment_value)::NUMERIC / COUNT(DISTINCT ood.customer_id), 2) AS avg_value_per_customer,
-	AVG(order_delivered_carrier_date - order_purchase_timestamp) AS avg_purchase_to_carrier,
-	AVG(order_delivered_customer_date - order_delivered_carrier_date) AS avg_carrier_to_customer,
 	ROUND(SUM(oord.review_score)::NUMERIC / COUNT(ood.order_id), 2) AS avg_ratings
 FROM olist_orders_dataset ood
 LEFT JOIN olist_order_payments_dataset oopd 
@@ -96,6 +48,47 @@ SELECT
 	ROUND(running_annual_rev::NUMERIC/running_annual_customer, 2) AS running_avg_order_value
 FROM b
 ORDER BY 1 DESC, 2 DESC;
+
+/* Sellers market insights */
+SELECT osd.seller_id, 
+	osd.seller_state,
+	osd.seller_city,
+	COUNT(DISTINCT ooid.order_id) AS total_order,
+	SUM(oopd.payment_value) AS total_rev,
+	ROUND(SUM(oopd.payment_value)::NUMERIC/COUNT(DISTINCT ooid.order_id), 2) AS avg_order_value,
+	ROUND(SUM(oord.review_score)::NUMERIC/COUNT(ooid.order_id), 2) AS avg_ratings
+FROM olist_sellers_dataset osd
+LEFT JOIN olist_order_items_dataset ooid
+ON osd.seller_id = ooid.seller_id
+LEFT JOIN olist_order_payments_dataset oopd
+ON ooid.order_id = oopd.order_id
+LEFT JOIN olist_order_reviews_dataset oord 
+ON ooid.order_id = oord.order_id 
+WHERE ooid.order_item_id = 1
+GROUP BY 1, 2, 3
+ORDER BY 5 DESC
+LIMIT 20;
+
+/* Product categories market insights */
+SELECT opd.product_category_name, 
+	pcnt.product_category_name_english,
+	COUNT(DISTINCT ooid.order_id) AS total_order, 
+	SUM(oopd.payment_value) AS total_rev,
+	ROUND(SUM(oopd.payment_value)::NUMERIC/COUNT(DISTINCT ooid.order_id), 2) AS avg_order_value,
+	ROUND((SUM(oord.review_score)::NUMERIC/COUNT(ooid.order_id)), 2) AS avg_ratings
+FROM olist_products_dataset opd 
+LEFT JOIN product_category_name_translation pcnt 
+ON opd.product_category_name = pcnt.product_category_name 
+LEFT JOIN olist_order_items_dataset ooid
+ON opd.product_id = ooid.product_id 
+LEFT JOIN olist_order_payments_dataset oopd
+ON ooid.order_id = oopd.order_id
+LEFT JOIN olist_order_reviews_dataset oord 
+ON ooid.order_id = oord.order_id 
+WHERE ooid.order_item_id = 1
+GROUP BY 1, 2
+ORDER BY 4 DESC
+LIMIT 20;
 
 /* Percentage of order status */
 SELECT order_status, ROUND(COUNT(order_status)::NUMERIC / 99441 * 100, 3) AS percentage FROM olist_orders_dataset ood 
